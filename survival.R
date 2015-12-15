83038fae84bfb40af8becaa5cdd8d0b8f856f97e
Simon Schwab <schw4b@gmail.com>
2015-12-15 17:02:35 +0100

    R analysis script for published dataset

--- survival.R
+++ survival.R
@@ -5,3 +5,26 @@
 # Gysin-Maillart A, Schwab S, Soravia LM, Megert M, & Michel K (2015).
 # A novel brief therapy for attempted suicide: Two year follow-up randomized
 # controlled study of the Attempted Suicide Short Intervention Program (ASSIP).
+
+# loading  data ----
+load("assip.RData")
+
+repeater = c(
+  as.character(mydata$repeater_t2), #  6 months
+  as.character(mydata$repeater_t3), # 12
+  as.character(mydata$repeater_t4), # 18
+  as.character(mydata$repeater_t5)) # 24 R cannot concat factors, conversion to char
+
+repeater = as.factor(repeater) == 'Suizidversuch (mind. 1)'
+group = rep(mydata$ITT,4) # repeat group coding for the 4 follow-up's
+time = c(rep(6,120), rep(12,120), rep(18,120), rep(24,120)) # follow-up month
+
+# survival (Kaplan-Meier) ----
+fit = survfit(Surv(time, repeater) ~ group, type="kaplan-meier")
+summary(fit)
+
+# group difference (Mantel-Haenszel) ----
+survdiff(Surv(time, repeater) ~ group, rho=0)
+# Cox hazard for discrete data ----
+hazard <- coxph(Surv(time, repeater) ~ group, ties="exact")
+summary(hazard)
