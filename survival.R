289bd39f4cd387dde260c331778885ee4717a708
Simon Schwab <schw4b@gmail.com>
2015-12-15 16:06:43 +0100

    Create survival.R

---
+++ survival.R
@@ -0,0 +1,7 @@
+# by Simon Schwab, schwab@puk.unibe.ch, 2015
+# University Hospital of Psychiatry, and University of Bern.
+
+# Statistical analysis protocol for the ASSIP trial
+# Gysin-Maillart A, Schwab S, Soravia LM, Megert M, & Michel K (2015).
+# A novel brief therapy for attempted suicide: Two year follow-up randomized
+# controlled study of the Attempted Suicide Short Intervention Program (ASSIP).
